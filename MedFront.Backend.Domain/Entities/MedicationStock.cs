68de16554e255507909b7a4c5b4a10220822a866
9Denys <dendanchenko28@gmail.com>
2025-12-26 18:13:55 +0200

    Fix nullable navigation properties in MedicationStock

--- MedFront.Backend.Domain/Entities/MedicationStock.cs
+++ MedFront.Backend.Domain/Entities/MedicationStock.cs
@@ -4,17 +4,15 @@ namespace MedFront.Backend.Domain.Entities
 {
     public class MedicationStock : BaseEntity
     {
-        public required Guid MedicationId { get; set; }
-        public required virtual Medication Medication { get; set; }
+        public Guid MedicationId { get; set; }
+        public virtual Medication? Medication { get; set; }
 
-        public required Guid WarehouseId { get; set; }
-        public required virtual Warehouse Warehouse { get; set; }
+        public Guid WarehouseId { get; set; }
+        public virtual Warehouse? Warehouse { get; set; }
 
         public int BoxQuantity { get; set; }
         public int StockNorm { get; set; }
-
         public DateOnly? ExpirationDate { get; set; }
-
-       
     }
+
 }
